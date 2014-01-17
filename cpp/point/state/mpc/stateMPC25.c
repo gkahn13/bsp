04b2a2b83d1e76f014cd37fec89c95ab9b261d8d
Gregory Kahn <gkahn13@gmail.com>
2014-01-17 11:16:25 -0800

    fixed state/mpc includes

--- cpp/point/state/mpc/stateMPC25.c
+++ cpp/point/state/mpc/stateMPC25.c
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "../include/stateMPC.h"
+#include "stateMPC.h"
 
 /* for square root */
 #include <math.h> 

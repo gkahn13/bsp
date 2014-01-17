bef64cb8219a244454db58fa2279a62d79da57b8
Gregory Kahn <gkahn13@gmail.com>
2014-01-17 10:50:54 -0800

    go go

--- cpp/point/belief/mpc/beliefPenaltyMPC30.c
+++ cpp/point/belief/mpc/beliefPenaltyMPC30.c
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "../include/beliefPenaltyMPC.h"
+#include "beliefPenaltyMPC.h"
 
 /* for square root */
 #include <math.h> 

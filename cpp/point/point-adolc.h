f21d219b6a30e416517b02e9ac9898acc0bcb86a
Gregory Kahn <gkahn13@gmail.com>
2014-01-23 16:23:09 -0800

    forgot to add point-state-adolc

---
+++ cpp/point/point-adolc.h
@@ -0,0 +1,516 @@
+#ifndef __POINT_ADOLC_H__
+#define __POINT_ADOLC_H__
+
+#include <fstream>
+#include <math.h>
+
+#include "util/matrix.h"
+#include "util/utils.h"
+//#include "util/Timer.h"
+#include "util/logging.h"
+//#include "util/invertMatrix.hpp"
+
+#include <Python.h>
+//#include <pythonrun.h>
+#include <boost/python.hpp>
+#include <boost/filesystem.hpp>
+
+namespace py = boost::python;
+
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/matrix_proxy.hpp>
+#include <boost/numeric/ublas/io.hpp>
+using namespace boost::numeric::ublas;
+
+#include <adolc/adolc.h>
+
+#define TIMESTEPS 15
+#define DT 1.0
+#define X_DIM 2
+#define U_DIM 2
+#define Z_DIM 2
+#define Q_DIM 2
+#define R_DIM 2
+
+#define S_DIM (((X_DIM+1)*X_DIM)/2)
+#define B_DIM (X_DIM+S_DIM)
+
+
+const double step = 1e-6;//0.0078125*0.0078125;
+
+matrix<double> x0(X_DIM,1);
+matrix<double> SqrtSigma0(X_DIM,X_DIM);
+matrix<double> xGoal(X_DIM,1);
+matrix<double> xMin(X_DIM,1), xMax(X_DIM,1);
+matrix<double> uMin(U_DIM,1), uMax(U_DIM,1);
+
+
+matrix<double> Q = identity_matrix<double>(Q_DIM);
+matrix<double> R = identity_matrix<double>(R_DIM);
+
+const int T = TIMESTEPS;
+const double INFTY = 1e10;
+
+const short int DOBS_DX_TAG = 1;
+const short int DOBS_DR_TAG = 2;
+const short int GRAD_COST_TAG = 3;
+const short int HESS_COST_TAG = 4;
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+const double alpha_belief = 10, alpha_final_belief = 10, alpha_control = 1, alpha_goal_state = 1;
+
+adouble *inputVars, *vars;
+std::vector<int> maskIndices;
+
+template<class TYPE>
+matrix<TYPE> zeroMatrix(int rows, int cols)
+{
+	matrix<TYPE> M(rows,cols);
+	for(int i=0; i < M.size1(); ++i)
+	{
+		for(int j=0; j < M.size2(); ++j)
+		{
+			M(i,j) = 0.0;
+		}
+	}
+	return M;
+}
+
+template<class TYPE>
+matrix<TYPE> identityMatrix(int size)
+{
+	matrix<TYPE> M = zeroMatrix<TYPE>(size, size);
+	for(int i=0; i < size; ++i) { M(i,i) = 1.0; }
+	return M;
+}
+
+template<class TYPE>
+matrix<TYPE> copyMatrix(matrix<TYPE> M)
+{
+	matrix<TYPE> Mcopy(M.size1(),M.size2());
+	for(int i=0; i < M.size1(); ++i)
+	{
+		for(int j=0; j < M.size2(); ++j)
+		{
+			Mcopy(i,j) = M(i,j);
+		}
+	}
+	return M;
+}
+
+template<class TYPE>
+void printMatrix(std::string name, matrix<TYPE> M)
+{
+	matrix<TYPE> transM = trans(M);
+	std::cout << name << std::endl;
+	for(int i=0; i < transM.size2(); ++i)
+	{
+		std::cout << column(transM,i) << std::endl;
+	}
+}
+
+template<class TYPE>
+void arrToMatrix(double** arr, matrix<TYPE>& M)
+{
+	for(int i=0; i < M.size1(); ++i)
+	{
+		for(int j=0; j < M.size2(); ++j)
+		{
+			M(i,j) = arr[i][j];
+		}
+	}
+}
+
+// converted
+template<class TYPE>
+matrix<TYPE> dynfunc(const matrix<TYPE>& x, const matrix<TYPE>& u, const matrix<TYPE>& q)
+{
+	matrix<TYPE> xNew = x + u*DT + 0.01*q;
+	return xNew;
+}
+
+
+// converted
+// Observation model
+template<class TYPE>
+matrix<TYPE> obsfunc(const matrix<TYPE>& x, const matrix<TYPE>& r)
+{
+	TYPE intensity = sqrt(0.5*0.5*x(0,0)*x(0,0) + 1e-6);
+	matrix<TYPE> z = x + intensity*r;
+	return z;
+}
+
+// converted (assuming no work needs to be done)
+// Jacobians: df(x,u,q)/dx, df(x,u,q)/dq
+// TODO: remove!
+void linearizeDynamics(const matrix<adouble>& x, const matrix<adouble>& u, const matrix<adouble>& q, matrix<adouble>& A, matrix<adouble>& M)
+{
+	//matrix<double> m(3,3), v(3,1);
+	//column(m,0) = column(v,0);
+
+	//A.reset();
+	matrix<adouble> xr(x), xl(x);
+	for (size_t i = 0; i < X_DIM; ++i) {
+		xr(i,0) += step; xl(i,0) -= step;
+		column(A, i) = column((dynfunc(xr, u, q) - dynfunc(xl, u, q)) / (xr(i,0) - xl(i,0)),0);
+		xr(i,0) = x(i,0); xl(i,0) = x(i,0);
+	}
+
+
+	//M.reset();
+	matrix<adouble> qr(q), ql(q);
+	for (size_t i = 0; i < Q_DIM; ++i) {
+		qr(i,0) += step; ql(i,0) -= step;
+		column(M, i) = column((dynfunc(x, u, qr) - dynfunc(x, u, ql)) / (qr(i,0) - ql(i,0)),0);
+		qr(i,0) = q(i,0); ql(i,0) = q(i,0);
+
+	}
+
+}
+
+void initAdolcMatrix(matrix<adouble>& mAdolc, matrix<double>& m)
+{
+	for(int i=0; i < mAdolc.size1(); ++i) {
+		for(int j=0; j < mAdolc.size2(); ++j) {
+			mAdolc(i,j) <<= m(i,j);
+		}
+	}
+}
+
+void retrieveAdolcMatrix(matrix<double>& m, matrix<adouble>& mAdolc)
+{
+	for(int i=0; i < m.size1(); ++i) {
+		for(int j=0; j < m.size2(); ++j) {
+			mAdolc(i,j) >>= m(i,j);
+		}
+	}
+}
+
+void finiteDiffJac(matrix<double>& x, matrix<double>& r)
+{
+	matrix<double> H = zeroMatrix<double>(Z_DIM,X_DIM);
+	matrix<double> xr(x), xl(x);
+	for (size_t i = 0; i < X_DIM; ++i) {
+		xr(i,0) += step; xl(i,0) -= step;
+		column(H, i) = column((obsfunc(xr, r) - obsfunc(xl, r)) / (xr(i,0) - xl(i,0)),0);
+		xr(i,0) = x(i,0); xl(i,0) = x(i,0);
+	}
+	printMatrix<double>("H finite diff", H);
+
+	matrix<double> N = zeroMatrix<double>(Z_DIM,R_DIM);
+	matrix<double> rr(x), rl(x);
+	for (size_t i = 0; i < R_DIM; ++i) {
+		rr(i,0) += step; rl(i,0) -= step;
+		column(N, i) = column((obsfunc(x, rr) - obsfunc(x, rl)) / (rr(i,0) - rl(i,0)),0);
+		rr(i,0) = r(i,0); rl(i,0) = r(i,0);
+	}
+	printMatrix<double>("N finite diff", N);
+}
+
+
+// converted (assuming no work needs to be done)
+// Jacobians: dh(x,r)/dx, dh(x,r)/dr
+template<class TYPE>
+void linearizeObservation(matrix<TYPE>& x, matrix<TYPE>& r, matrix<TYPE>& H, matrix<TYPE>& N)
+{
+	matrix<TYPE> x_double(X_DIM,1), r_double(R_DIM,1);
+	if (std::is_same<TYPE,adouble>::value) {
+		// extract value from x and r to compute jacs
+		for(int i=0; i < X_DIM; ++i) { x_double(i,0) = x(i,0).value(); }
+		for(int i=0; i < R_DIM; ++i) { r_double(i,0) = r(i,0).value(); }
+	} else {
+		x_double = x;
+		r_double = r;
+	}
+
+	LOG_DEBUG("inside linearizeObservation adolc");
+	matrix<adouble> xAdolc(X_DIM,1);
+	matrix<double> xObs(Z_DIM,1);
+
+	trace_on(DOBS_DX_TAG);
+	initAdolcMatrix(xAdolc, x_double);
+	matrix<adouble> xObsAdolc = obsfunc<adouble>(xAdolc, r_double);
+	retrieveAdolcMatrix(xObs, xObsAdolc);
+	trace_off(DOBS_DX_TAG);
+
+	double* x_arr = new double[X_DIM];
+	std::copy(x_double.begin1(), x_double.end1(), x_arr);
+	double** dobs_dx = new double*[Z_DIM];
+	for(int i=0; i < Z_DIM; ++i) { dobs_dx[i] = new double[X_DIM]; }
+
+	jacobian(DOBS_DX_TAG, Z_DIM, X_DIM, x_arr, dobs_dx);
+	arrToMatrix(dobs_dx, H);
+
+	matrix<adouble> rAdolc(R_DIM,1);
+	matrix<double> rObs(Z_DIM,1);
+
+	trace_on(DOBS_DR_TAG);
+	initAdolcMatrix(rAdolc, r_double);
+	matrix<adouble> rObsAdolc = obsfunc<adouble>(x_double, rAdolc);
+	retrieveAdolcMatrix(rObs, rObsAdolc);
+	trace_off(DOBS_DR_TAG);
+
+	double* r_arr = new double[R_DIM];
+	std::copy(r_double.begin1(), r_double.end1(), r_arr);
+	double** dobs_dr = new double*[Z_DIM];
+	for(int i=0; i < Z_DIM; ++i) { dobs_dr[i] = new double[R_DIM]; }
+
+	jacobian(DOBS_DR_TAG, Z_DIM, R_DIM, r_arr, dobs_dr);
+	arrToMatrix(dobs_dr, N);
+
+	//finiteDiffJac(x,r);
+}
+
+
+
+
+// converted (assuming no work needs to be done)
+// Switch between belief vector and matrices
+template<class TYPE>
+void unVec(const matrix<TYPE>& b, matrix<TYPE>& x, matrix<TYPE>& SqrtSigma) {
+	matrix<TYPE> x_tmp(X_DIM,1);
+	for(int i=0; i < X_DIM; ++i) { x_tmp(i,0) = b(i,0); };
+	x = x_tmp;
+
+	size_t idx = X_DIM;
+	for (size_t j = 0; j < X_DIM; ++j) {
+		for (size_t i = j; i < X_DIM; ++i) {
+			SqrtSigma(i,j) = b(idx,0);
+			SqrtSigma(j,i) = b(idx,0);
+			++idx;
+		}
+	}
+}
+
+// converted (assuming no work needs to be done)
+template<class TYPE>
+void vec(const matrix<TYPE>& x, const matrix<TYPE>& SqrtSigma, matrix<TYPE>& b) {
+	for(int i=0; i < X_DIM; ++i) { b(i,0) = x(i,0); }
+	size_t idx = X_DIM;
+	for (size_t j = 0; j < X_DIM; ++j) {
+		for (size_t i = j; i < X_DIM; ++i) {
+			b(idx,0) = 0.5 * (SqrtSigma(i,j) + SqrtSigma(j,i));
+			++idx;
+		}
+	}
+}
+
+template<class TYPE>
+matrix<TYPE> mdivide(matrix<TYPE> A, matrix<TYPE> B)
+{
+	// Cholesky factorization A = L*~L
+	// check if symmetric
+	int size = A.size1();
+	matrix<TYPE> L = zeroMatrix<TYPE>(size,size);
+	for (int i = 0; i < size; ++i)
+	{
+		for (int j = i; j < size; ++j)
+		{
+			TYPE sum = A(j, i);
+			for (int k = 0; k < i; ++k)
+			{
+				sum -= L(j, k) * L(i, k);
+			}
+			if (i == j)
+			{
+				L(i, i) = sqrt(sum);
+			}
+			else
+			{
+				L(j, i) = sum / L(i, i);
+			}
+		}
+	}
+
+	int ncols = B.size2();
+	// Backward and forward substitution
+	matrix<TYPE> M(size, ncols);
+	for (int i = 0; i < size; ++i)
+	{
+		for (int k = 0; k < ncols; ++k)
+		{
+			TYPE sum = B(i, k);
+			for (int j = 0; j < i; ++j)
+			{
+				sum -= L(i, j) * M(j, k);
+			}
+			M(i, k) = sum / L(i, i);
+		}
+	}
+	for (int i = size - 1; i != -1; --i)
+	{
+		for (int k = 0; k < ncols; ++k)
+		{
+			TYPE sum = M(i, k);
+			for (int j = i + 1; j < size; ++j)
+			{
+				sum -= L(j, i) * M(j, k);
+			}
+			M(i, k) = sum / L(i, i);
+		}
+	}
+	return M;
+}
+
+/*
+// TODO: can't do sqrt(Sigma), so can't comlete function
+// converted (same except for A, M)
+// Belief dynamics
+matrix<adouble> beliefDynamics(const matrix<adouble>& b, const matrix<adouble>& u) {
+	matrix<adouble> x(X_DIM,1);
+	matrix<adouble> SqrtSigma(X_DIM,X_DIM);
+	unVec(b, x, SqrtSigma);
+
+	matrix<adouble> Sigma(X_DIM,X_DIM);
+	Sigma = prod(SqrtSigma, SqrtSigma);
+
+
+	//linearizeDynamics(x, u, zeros<Q_DIM,1>(), A, M);
+	matrix<adouble> A = identityMatrix(X_DIM);
+	matrix<adouble> M = DT*identityMatrix(U_DIM);
+
+	x = dynfunc(x, u, zeroMatrix(Q_DIM,1));
+	Sigma = prod(A, matrix<adouble>(prod(Sigma,trans(A)))) + prod(M,trans(M));
+
+	matrix<adouble> H(Z_DIM,X_DIM);
+	matrix<adouble> N(Z_DIM,R_DIM);
+	matrix<adouble> r(R_DIM,1);
+	linearizeObservation(x, r, H, N);
+
+	matrix<adouble> prevInv = prod(H, matrix<adouble>(prod(Sigma,trans(H))));
+	prevInv = prevInv + prod(N,trans(N));
+
+	matrix<adouble> prevK = prod(Sigma, trans(H));
+	matrix<adouble> K = mdivide(prevK, prevInv);
+	//matrix<adouble> K = Sigma*~H/(H*Sigma*~H + N*~N);
+
+	matrix<adouble> prevSigma = identityMatrix(X_DIM) - prod(K,H);
+	Sigma = prod(prevSigma, Sigma);
+	//Sigma = (identity<X_DIM>() - K*H)*Sigma;
+
+	//matrix<adouble> b_g(B_DIM,1);
+	//vec(x, sqrt(Sigma), b_g);
+
+	return b;
+
+
+	//return b_g;
+}
+*/
+
+template<class TYPE>
+matrix<TYPE> EKF(const matrix<TYPE>& x_t, const matrix<TYPE>& u_t, const matrix<TYPE>& q_t, const matrix<TYPE>& r_t, const matrix<TYPE>& Sigma_t)
+{
+	printMatrix("x_t",x_t);
+	printMatrix("u_t",u_t);
+	//printMatrix("q_t",q_t);
+	//printMatrix("r_t",r_t);
+	printMatrix("Sigma_t",Sigma_t);
+
+	//computeDynJacobians(x_t, u_t, q_t, out A, out M);
+	matrix<double> A = identityMatrix<double>(X_DIM); // d(dynfunc)/dx
+	matrix<double> M = .01*identityMatrix<double>(U_DIM); // d(dynfunc)/dm (noise)
+
+
+	printMatrix("A",A);
+	printMatrix("M",M);
+
+	matrix<TYPE> Sigma_tp1 = prod(((matrix<TYPE>)prod(A, Sigma_t)), trans(A)) + prod(((matrix<TYPE>)prod(M, Q)), trans(M));
+
+	printMatrix("Sigma_tp1",Sigma_tp1);
+
+	LOG_DEBUG("before dynfunc");
+	matrix<TYPE> x_tp1 = dynfunc<TYPE>(x_t, u_t, zeroMatrix<double>(Q_DIM,1));
+
+	printMatrix("x_tp1",x_tp1);
+
+	matrix<TYPE> H = zeroMatrix<TYPE>(Z_DIM,X_DIM);
+	matrix<TYPE> N = zeroMatrix<TYPE>(Z_DIM,R_DIM);
+	//matrix<adouble> r = zeroMatrix(R_DIM,1); // correct?
+	LOG_DEBUG("before linearize observation");
+
+	matrix<TYPE> r = zeroMatrix<TYPE>(R_DIM,1);
+	linearizeObservation<TYPE>(x_tp1, r, H, N);
+
+	printMatrix("H",H);
+	printMatrix("N",N);
+
+
+	LOG_DEBUG("before K1, K2");
+	//printMatrix("Sigma_tp1",Sigma_tp1);
+	//printMatrix("trans(H)",trans(H));
+	matrix<TYPE> K1 = prod(Sigma_tp1, trans(H));
+	matrix<TYPE> K2 = prod(((matrix<TYPE>)prod(H, Sigma_tp1)), trans(H)) + prod(((matrix<TYPE>)prod(N, R)), trans(N));
+
+	LOG_DEBUG("before K");
+	matrix<TYPE> K = trans(mdivide<TYPE>(trans(K2), trans(K1)));
+
+	Sigma_tp1 = prod(((matrix<TYPE>)(identityMatrix<double>(X_DIM) - prod(K,H))), Sigma_tp1);
+
+	LOG_DEBUG("before b_tp1");
+	matrix<TYPE> b_tp1(B_DIM,1);
+	vec<TYPE>(x_tp1, Sigma_tp1, b_tp1);
+	return b_tp1;
+}
+
+template<class TYPE>
+TYPE costfunc(const matrix<TYPE>& X, const matrix<TYPE>& U, const matrix<TYPE>& q, const matrix<TYPE>& r, const matrix<TYPE>& Sigma_0)
+{
+	LOG_DEBUG("costfunc");
+	TYPE cost = 0;
+
+	matrix<TYPE> x_tp1(X_DIM,1);
+	column(x_tp1,0) = column(X,0);
+	matrix<TYPE> Sigma_t(X_DIM,X_DIM), Sigma_tp1(X_DIM,X_DIM);
+	Sigma_t = Sigma_0;
+
+	matrix<TYPE> Ucol(U_DIM,1), Xcol(X_DIM,1);
+	for (int t = 0; t < T - 1; ++t)
+	{
+		LOG_DEBUG("EKF iteration %d",t);
+		for(int i=0; i < X_DIM; ++i) {
+			cost = cost + alpha_belief * Sigma_t(i,i);
+		}
+		column(Ucol,0) = column(U,t);
+		cost = cost + alpha_control * (prod(trans(Ucol), Ucol))(0,0);
+
+		column(Xcol,0) = column(X,t);
+		LOG_DEBUG("before EKF");
+		matrix<TYPE> b_tp1 = EKF<TYPE>(Xcol, Ucol, q, r, Sigma_t);
+		LOG_DEBUG("after EKF");
+		unVec<TYPE>(b_tp1, x_tp1, Sigma_t);
+	}
+
+	for(int i=0; i < X_DIM; ++i) {
+		cost = cost + alpha_final_belief * Sigma_t(i,i);
+	}
+
+	return cost;
+}
+
+
+/*
+// converted (assuming no work needs to be done)
+void setupDstarInterface(std::string mask) {
+	std::stringstream ss(mask);
+	int val, i=0;
+	while (ss >> val) {
+		if (val == 1) {
+			maskIndices.push_back(i);
+		}
+		i++;
+	}
+
+	inputVars = new double[i];
+}
+
+// converted
+void pythonDisplayTrajectory(std::vector< matrix<adouble> >& B, std::vector< matrix<adouble> >& U)
+{
+	// no python for this example
+	// don't use
+}
+*/
+#endif
---
+++ cpp/point/state/point-state-adolc.cpp
@@ -0,0 +1,542 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <iomanip>
+
+#include "../point-adolc.h"
+
+#include "util/matrix.h"
+#include "util/utils.h"
+#include "util/Timer.h"
+#include "util/logging.h"
+//#include "util/adolcsupport.h"
+
+#include <Python.h>
+//#include <pythonrun.h>
+#include <boost/python.hpp>
+#include <boost/preprocessor/iteration/local.hpp>
+#include <boost/preprocessor/arithmetic/sub.hpp>
+#include <boost/filesystem.hpp>
+
+namespace py = boost::python;
+
+
+#include <boost/numeric/ublas/matrix.hpp>
+using namespace boost::numeric::ublas;
+
+#include <adolc/adolc.h>
+
+extern "C" {
+//#include "../sym/state-symeval.h"
+#include "stateMPC.h"
+}
+
+namespace cfg {
+const double improve_ratio_threshold = .1;
+const double min_approx_improve = 1e-2;
+const double min_trust_box_size = 1e-2;
+const double trust_shrink_ratio = .1;
+const double trust_expand_ratio = 1.5;
+}
+
+// stateMPC vars
+stateMPC_FLOAT **H, **f, **lb, **ub, **C, **e, **z;
+
+
+void setupStateMPCVars(stateMPC_params& problem, stateMPC_output& output)
+{
+	// inputs
+	H = new stateMPC_FLOAT*[T];
+	f = new stateMPC_FLOAT*[T];
+	lb = new stateMPC_FLOAT*[T];
+	ub = new stateMPC_FLOAT*[T];
+	C = new stateMPC_FLOAT*[T-1];
+	e = new stateMPC_FLOAT*[T];
+
+	// output
+	z = new stateMPC_FLOAT*[T];
+
+#define SET_VARS(n)    \
+		H[ BOOST_PP_SUB(n,1) ] = problem.H##n ;  \
+		f[ BOOST_PP_SUB(n,1) ] = problem.f##n ;  \
+		lb[ BOOST_PP_SUB(n,1) ] = problem.lb##n ;	\
+		ub[ BOOST_PP_SUB(n,1) ] = problem.ub##n ;	\
+		C[ BOOST_PP_SUB(n,1) ] = problem.C##n ; \
+		e[ BOOST_PP_SUB(n,1) ] = problem.e##n ;  \
+		z[ BOOST_PP_SUB(n,1) ] = output.z##n ;
+
+#define BOOST_PP_LOCAL_MACRO(n) SET_VARS(n)
+#define BOOST_PP_LOCAL_LIMITS (1, TIMESTEPS-1)
+#include BOOST_PP_LOCAL_ITERATE()
+
+#define SET_LAST_VARS(n)    \
+		H[ BOOST_PP_SUB(n,1) ] = problem.H##n ;  \
+		f[ BOOST_PP_SUB(n,1) ] = problem.f##n ;  \
+		lb[ BOOST_PP_SUB(n,1) ] = problem.lb##n ;	\
+		ub[ BOOST_PP_SUB(n,1) ] = problem.ub##n ;	\
+		e[ BOOST_PP_SUB(n,1) ] = problem.e##n ;  \
+		z[ BOOST_PP_SUB(n,1) ] = output.z##n ;
+
+#define BOOST_PP_LOCAL_MACRO(n) SET_LAST_VARS(n)
+#define BOOST_PP_LOCAL_LIMITS (TIMESTEPS, TIMESTEPS)
+#include BOOST_PP_LOCAL_ITERATE()
+
+}
+
+
+void cleanup()
+{
+	delete[] inputVars;
+	delete[] vars;
+
+	delete[] H;
+	delete[] f;
+	delete[] lb;
+	delete[] ub;
+	delete[] C;
+	delete[] e;
+	delete[] z;
+}
+
+void initVarVals(const matrix<adouble>& X, const matrix<adouble>& U)
+{
+	int idx = 0;
+	for (int t = 0; t < T; ++t) {
+		for (int i = 0; i < X_DIM; ++i) {
+			inputVars[idx++] = X(t,i);
+		}
+	}
+	for (int t = 0; t < (T - 1); ++t) {
+		for (int i = 0; i < U_DIM; ++i) {
+			inputVars[idx++] = U(t,i);
+		}
+	}
+	for (int i = 0; i < (Q_DIM+R_DIM); ++i) {
+		inputVars[idx++] = 0;
+	}
+	for (int i = 0; i < (X_DIM+X_DIM); ++i) {
+		inputVars[idx++] = SqrtSigma0(i,0);
+	}
+	inputVars[idx++] = alpha_belief; inputVars[idx++] = alpha_control; inputVars[idx++] = alpha_final_belief;
+
+	int nvars = (int)maskIndices.size();
+	//vars = new double[nvars];
+
+	// For evaluation
+	idx = 0;
+	for (int i = 0; i < nvars; ++i) {
+		vars[idx++] = inputVars[maskIndices[i]];
+	}
+}
+
+inline void forcePsdHessian() {
+	// zero out negative diagonal entries
+	for(int t = 0; t < T-1; ++t) {
+		for(int i = 0; i < (X_DIM+U_DIM); ++i) {
+			H[t][i] = (H[t][i] < 0) ? 0 : H[t][i];
+		}
+	}
+
+	for(int i = 0; i < X_DIM; ++i) {
+		H[T-1][i] = (H[T-1][i] < 0) ? 0 : H[T-1][i];
+	}
+}
+
+bool isValidInputs(double *result) {
+
+	//stateMPC_FLOAT **H, **f, **lb, **ub, **C, **e, **z;
+	for(int t = 0; t < T-1; ++t) {
+		std::cout << "t: " << t << std::endl << std::endl;
+
+
+
+		std::cout << "H: " << std::endl;
+		for(int i = 0; i < 4; ++i) {
+			std::cout << H[t][i] << std::endl;
+		}
+
+		std::cout << std::endl << std::endl;
+	}
+	return true;
+}
+
+
+double stateCollocation(matrix<double> X, matrix<double> U, const stateMPC_params& problem, const stateMPC_output& output, const stateMPC_info& info)
+{
+	int maxIter = 100;
+	double Xeps = 1;
+	double Ueps = 1;
+
+	// box constraint around goal
+	double delta = 0.01;
+	matrix<double> x0(X_DIM,1);
+	column(x0,0) = column(X,0);
+
+	double prevcost = INFTY, optcost;
+	double merit, model_merit, new_merit;
+	double approx_merit_improve, exact_merit_improve, merit_improve_ratio;
+	double constant_cost, hessian_constant, jac_constant;
+
+	int dim = T*X_DIM + (T-1)*U_DIM;
+	double* resultCost = new double;
+	double* resultCostGradDiagHess = new double[2*dim + 1];
+
+	double Hzbar[X_DIM+U_DIM];
+
+	int nvars = (int)maskIndices.size();
+	vars = new adouble[nvars];
+
+	//initVarVals(X, U);
+
+
+	//evalCost(resultCost, vars); // TODO: replace with cost function
+	//prevcost = resultCost[0];
+	matrix<double> q(Q_DIM,1), r(R_DIM,1);
+	prevcost = costfunc<double>(X, U, q, r, SqrtSigma0);
+
+	std::cout << "prevcost: " << prevcost << std::endl;
+
+	LOG_DEBUG("Initialization trajectory cost: %4.10f", prevcost);
+
+	matrix<adouble> Xopt(X_DIM, T);
+	matrix<adouble> Uopt(U_DIM, T-1);
+
+	double **aXU = new double*[T];
+	for(int t=0; t < T; ++ t)
+	{
+		aXU[t] = new double[X_DIM+U_DIM];
+		for(int x_idx=0; x_idx < X_DIM; ++x_idx) { aXU[t][x_idx] = X(t,x_idx); }
+		for(int u_idx=0; u_idx < U_DIM; ++u_idx) { aXU[t][X_DIM+u_idx] = U(t,u_idx); }
+	}
+
+
+	bool solution_accepted = true;
+
+	for(int it = 0; it < maxIter; ++it)
+	{
+		LOG_DEBUG("\nIter: %d", it);
+
+		if (solution_accepted) {
+			//initVarVals(X, U);
+			//evalCostGradDiagHess(resultCostGradDiagHess, vars);
+			//merit = resultCostGradDiagHess[0];
+
+			matrix<adouble> XAdolc(X_DIM,T);
+			matrix<adouble> UAdolc(U_DIM,T-1);
+			double merit;
+			adouble meritAdolc;
+
+			trace_on(GRAD_COST_TAG);
+			initAdolcMatrix(XAdolc, X);
+			initAdolcMatrix(UAdolc, U);
+			meritAdolc = costfunc<adouble>(XAdolc, UAdolc, q, r, SqrtSigma0);
+			meritAdolc >>= merit;
+			trace_off(GRAD_COST_TAG);
+
+			double* grad_arr = new double[X_DIM*T + U_DIM*(T-1)];
+			double* XU_arr = new double[X_DIM*T + U_DIM*(T-1)];
+			std::copy(X.begin1(), X.end1(), XU_arr);
+			std::copy(U.begin1(), U.end1(), XU_arr + (X.end1()-X.begin1()));
+			gradient(GRAD_COST_TAG, X_DIM*T + U_DIM*(T-1), XU_arr, grad_arr);
+
+			exit(0);
+
+			/*
+
+
+			// evaluate constant cost term (omitted from optimization)
+			// Need to compute:
+			// ~z_bar * H * z_bar (easy to compute since H is diagonal)
+			// -f * z_bar
+			constant_cost = 0;
+			hessian_constant = 0;
+			jac_constant = 0;
+
+			// compute Hessian first
+			// so can force it to be PSD
+			for (int t = 0; t < T-1; ++t) {
+				H[t][0] = resultCostGradDiagHess[1+dim+t*X_DIM];
+				H[t][1] = resultCostGradDiagHess[1+dim+t*X_DIM+1];
+				H[t][2] = resultCostGradDiagHess[1+dim+T*X_DIM+t*U_DIM];
+				H[t][3] = resultCostGradDiagHess[1+dim+T*X_DIM+t*U_DIM+1];
+			}
+			H[T-1][0] = resultCostGradDiagHess[1+dim+(T-1)*X_DIM];
+			H[T-1][1] = resultCostGradDiagHess[1+dim+(T-1)*X_DIM+1];
+
+			forcePsdHessian();
+
+			for (int t = 0; t < T-1; ++t)
+			{
+				Matrix<X_DIM>& xt = X[t];
+				Matrix<U_DIM>& ut = U[t];
+
+				Matrix<X_DIM+U_DIM> zbar;
+				zbar.insert(0,0,xt);
+				zbar.insert(X_DIM,0,ut);
+
+				for(int i = 0; i < (X_DIM+U_DIM); ++i) {
+					Hzbar[i] = H[t][i]*zbar[i];
+				}
+
+				f[t][0] = resultCostGradDiagHess[1+t*X_DIM];
+				f[t][1] = resultCostGradDiagHess[1+t*X_DIM+1];
+				f[t][2] = resultCostGradDiagHess[1+T*X_DIM+t*U_DIM];
+				f[t][3] = resultCostGradDiagHess[1+T*X_DIM+t*U_DIM+1];
+
+				for(int i = 0; i < (X_DIM+U_DIM); ++i) {
+					hessian_constant += H[t][i]*zbar[i]*zbar[i];
+					jac_constant += -(f[t][i]*zbar[i]);
+				}
+
+				for(int i = 0; i < (X_DIM+U_DIM); ++i) {
+					f[t][i] -= Hzbar[i];
+				}
+
+				// TODO: move following CMat code outside, is same every iteration
+				Matrix<X_DIM,X_DIM+U_DIM> CMat;
+
+				CMat.insert<X_DIM,X_DIM>(0,0,identity<X_DIM>());
+				CMat.insert<X_DIM,U_DIM>(0,X_DIM,DT*identity<U_DIM>());
+				int idx = 0;
+				for(int c = 0; c < (X_DIM+U_DIM); ++c) {
+					for(int r = 0; r < X_DIM; ++r) {
+						C[t][idx++] = CMat[c + r*(X_DIM+U_DIM)];
+					}
+				}
+
+				if (t == 0) {
+					e[t][0] = x0[0]; e[t][1] = x0[1];
+				} else {
+					e[t][0] = 0; e[t][1] = 0;
+				}
+				*/
+
+			} //setting up problem
+
+			/*
+			// Last stage
+			Matrix<X_DIM>& xT = X[T-1];
+
+			f[T-1][0] = resultCostGradDiagHess[1+(T-1)*X_DIM];
+			f[T-1][1] = resultCostGradDiagHess[1+(T-1)*X_DIM+1];
+
+			for(int i = 0; i < X_DIM; ++i) {
+				hessian_constant += H[T-1][i]*xT[i]*xT[i];
+				jac_constant += -(f[T-1][i]*xT[i]);
+			}
+
+			for(int i = 0; i < X_DIM; ++i) {
+				Hzbar[i] = H[T-1][i]*xT[i];
+				f[T-1][i] -= Hzbar[i];
+			}
+
+			e[T-1][0] = 0; e[T-1][1] = 0;
+
+			constant_cost = 0.5*hessian_constant + jac_constant + resultCostGradDiagHess[0];
+			*/
+		} // end solution_accepted
+
+
+		/*
+		// set trust region bounds based on current trust region size
+		for (int t = 0; t < T-1; ++t)
+		{
+			Matrix<X_DIM>& xt = X[t];
+			Matrix<U_DIM>& ut = U[t];
+
+			// Fill in lb, ub, C, e
+			lb[t][0] = MAX(xMin[0], xt[0] - Xeps);
+			lb[t][1] = MAX(xMin[1], xt[1] - Xeps);
+			lb[t][2] = MAX(uMin[0], ut[0] - Ueps);
+			lb[t][3] = MAX(uMin[1], ut[1] - Ueps);
+
+			ub[t][0] = MIN(xMax[0], xt[0] + Xeps);
+			ub[t][1] = MIN(xMax[1], xt[1] + Xeps);
+			ub[t][2] = MIN(uMax[0], ut[0] + Ueps);
+			ub[t][3] = MIN(uMax[1], ut[1] + Ueps);
+		} //setting up problem
+
+		// Fill in lb, ub, C, e
+		Matrix<X_DIM>& xT = X[T-1];
+		lb[T-1][0] = MAX(xGoal[0] - delta, xT[0] - Xeps);
+		lb[T-1][1] = MAX(xGoal[1] - delta, xT[1] - Xeps);
+
+		ub[T-1][0] = MIN(xGoal[0] + delta, xT[0] + Xeps);
+		ub[T-1][1] = MIN(xGoal[1] + delta, xT[1] + Xeps);
+
+		// Verify problem inputs
+
+		//if (!isValidInputs(result)) {
+		//	std::cout << "Inputs are not valid!" << std::endl;
+		//	exit(0);
+		//}
+
+
+		int exitflag = stateMPC_solve(&problem, &output, &info);
+		if (exitflag == 1) {
+			for(int t = 0; t < T-1; ++t) {
+				Matrix<X_DIM>& xt = Xopt[t];
+				Matrix<U_DIM>& ut = Uopt[t];
+
+				for(int i = 0; i < X_DIM; ++i) {
+					xt[i] = z[t][i];
+				}
+				for(int i = 0; i < U_DIM; ++i) {
+					ut[i] = z[t][X_DIM+i];
+				}
+				optcost = info.pobj;
+			}
+			Matrix<X_DIM>& xt = Xopt[T-1];
+			xt[0] = z[T-1][0]; xt[1] = z[T-1][1];
+		}
+		else {
+			LOG_FATAL("Some problem in solver");
+			std::exit(-1);
+		}
+
+		model_merit = optcost + constant_cost; // need to add constant terms that were dropped
+
+		initVarVals(Xopt, Uopt);
+		evalCost(resultCost, vars);
+		new_merit = resultCost[0];
+
+		LOG_DEBUG("merit: %f", merit);
+		LOG_DEBUG("model_merit: %f", model_merit);
+		LOG_DEBUG("new_merit: %f", new_merit);
+		LOG_DEBUG("constant cost term: %f", constant_cost);
+
+		approx_merit_improve = merit - model_merit;
+		exact_merit_improve = merit - new_merit;
+		merit_improve_ratio = exact_merit_improve / approx_merit_improve;
+
+		LOG_DEBUG("approx_merit_improve: %f", approx_merit_improve);
+		LOG_DEBUG("exact_merit_improve: %f", exact_merit_improve);
+		LOG_DEBUG("merit_improve_ratio: %f", merit_improve_ratio);
+
+		if (approx_merit_improve < -1e-5) {
+			LOG_ERROR("Approximate merit function got worse: %f", approx_merit_improve);
+			LOG_ERROR("Failure!");
+			delete resultCost;
+			delete[] resultCostGradDiagHess;
+			return INFTY;
+		} else if (approx_merit_improve < cfg::min_approx_improve) {
+			LOG_DEBUG("Converged: improvement small enough");
+			X = Xopt; U = Uopt;
+			solution_accepted = true;
+			break;
+		} else if ((exact_merit_improve < 0) || (merit_improve_ratio < cfg::improve_ratio_threshold)) {
+			LOG_DEBUG("Shrinking trust region size to: %2.6f %2.6f", Xeps, Ueps);
+			Xeps *= cfg::trust_shrink_ratio;
+			Ueps *= cfg::trust_shrink_ratio;
+			solution_accepted = false;
+		} else {
+			LOG_DEBUG("Accepted, Increasing trust region size to:  %2.6f %2.6f", Xeps, Ueps);
+			// expand Xeps and Ueps
+			Xeps *= cfg::trust_expand_ratio;
+			Ueps *= cfg::trust_expand_ratio;
+			X = Xopt; U = Uopt;
+			prevcost = optcost;
+			solution_accepted = true;
+		}
+	}
+
+	initVarVals(X, U);
+	evalCost(resultCost, vars);
+	optcost = resultCost[0];
+
+	delete resultCost;
+	delete[] resultCostGradDiagHess;
+
+	return optcost;
+	*/
+	return 0;
+}
+
+int main(int argc, char* argv[])
+{
+	x0(0,0) = -3.5; x0(1,0) = 2;
+	SqrtSigma0 = identity_matrix<double>(X_DIM);
+	xGoal(0,0) = -3.5; xGoal(1,0) = -2;
+
+	xMin(0,0) = -5; xMin(1,0) = -3;
+	xMax(0,0) = 5;  xMax(1,0) = 3;
+
+	for(int i=0; i < U_DIM; ++i) { uMin(i,0) = -1; uMax(i,0) = 1; }
+
+	matrix<double> uinit = (xGoal - x0) / (T-1);
+
+	matrix<double> U(U_DIM,T-1);
+	for(int t=0; t < T-1; ++t) { column(U,t) = column(uinit,0); }
+
+	printMatrix("initial U", U);
+
+
+	matrix<double> X = zeroMatrix<double>(X_DIM,T);
+
+	printMatrix("pre initial X", X);
+
+	column(X,0) = column(x0,0);
+	matrix<double> Xcol(X_DIM,1), Ucol(U_DIM,1);
+	for (int t = 0; t < T-1; ++t) {
+		column(Xcol,0) = column(X,t);
+		column(Ucol,0) = column(U,t);
+		matrix<double>q = zeroMatrix<double>(Q_DIM,1);
+
+		matrix<double> x_tp1 = dynfunc<double>(Xcol, Ucol, q);
+		column(X,t+1) = column(x_tp1,0);
+	}
+
+	printMatrix("initial X", X);
+	//setupDstarInterface(std::string(getMask()));
+
+	stateMPC_params problem;
+	stateMPC_output output;
+	stateMPC_info info;
+
+	setupStateMPCVars(problem, output);
+
+	util::Timer solveTimer;
+	util::Timer_tic(&solveTimer);
+
+	// compute cost for the trajectory
+
+	//for(int i = 0; i < 100000; ++i) {
+	//	for (int t = 0; t < T-1; ++t) {
+	//		X[t+1] = dynfunc(X[t], U[t], zeros<Q_DIM,1>());
+	//	}
+	//}
+
+
+	std::cout << "before state collocation " << std::endl;
+
+	double cost = stateCollocation(X, U, problem, output, info);
+
+	std::cout << "after state collocation" << std::endl;
+
+	double solvetime = util::Timer_toc(&solveTimer);
+	LOG_INFO("Cost: %4.10f", cost);
+	LOG_INFO("Solve time: %5.3f ms", solvetime*1000);
+
+	/*
+
+	Matrix<B_DIM> binit = zeros<B_DIM>();
+	std::vector<Matrix<B_DIM> > B(T, binit);
+
+	vec(X[0], SqrtSigma0, B[0]);
+	for (size_t t = 0; t < T-1; ++t) {
+		B[t+1] = beliefDynamics(B[t], U[t]);
+	}
+
+	pythonDisplayTrajectory(B, U);
+
+	cleanup();
+
+	int k;
+	std::cin >> k;
+	*/
+	//CAL_End();
+	return 0;
+
+
+}

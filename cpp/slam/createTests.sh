54a73208ed501a992a6b1e82c8cf73aeb91e5b51
gkahn <gkahn13@gmail.com>
2014-03-10 09:03:20 -0700

    finished all tests for traj state and control 25 runs each

--- cpp/slam/createTests.sh
+++ cpp/slam/createTests.sh
@@ -1,8 +1,8 @@
-num_runs_per_test=20
+num_runs_per_test=25
 landmarkDir=landmarkTextFiles
 mkdir -p $landmarkDir
 
-for num_landmarks in {3,4,5,6};
+for num_landmarks in {3,4,5,6,10,15,20,25,30,35,40,45,50};
 do
     python generate_landmarks.py $num_landmarks $num_runs_per_test
     cp landmarks.txt ${landmarkDir}/landmarks-${num_landmarks}.txt
